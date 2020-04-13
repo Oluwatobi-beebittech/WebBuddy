429edfd7ed6d164c336dfae4a23a9003d6b30c63
Oluwatobi AKanji <akanjioluwatobishadrach@yahoo.com>
2020-04-13 23:56:47 +0100

    Changed the navbar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counterComponent';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./css/main.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counterComponent";
 
 ReactDOM.render(
   <React.StrictMode>
     <Counter />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
