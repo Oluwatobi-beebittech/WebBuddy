e4a6b890195cad06029a51795d40f59555cb0d81
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2020-04-14 22:25:11 +0100

    modified link

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -4,21 +4,21 @@ class Counter extends Component {
     return (
       <div>
         <div class="row justify-content-center">
-          <a href = "#">
-          <svg
-            class="bi bi-plus-square-fill"
-            width="2em"
-            height="2em"
-            viewBox="0 0 16 16"
-            fill="#008aff"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm6.5 4a.5.5 0 00-1 0v3.5H4a.5.5 0 000 1h3.5V12a.5.5 0 001 0V8.5H12a.5.5 0 000-1H8.5V4z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <a href="#" target="">
+            <svg
+              class="bi bi-plus-square-fill"
+              width="2em"
+              height="2em"
+              viewBox="0 0 16 16"
+              fill="#008aff"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm6.5 4a.5.5 0 00-1 0v3.5H4a.5.5 0 000 1h3.5V12a.5.5 0 001 0V8.5H12a.5.5 0 000-1H8.5V4z"
+                clip-rule="evenodd"
+              />
+            </svg>
           </a>
         </div>
         <div class="container">ds</div>
