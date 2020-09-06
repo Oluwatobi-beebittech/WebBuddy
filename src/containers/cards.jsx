38d11401d642d43ac1cc8e793cbafcf00aeb5e20
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2020-09-06 16:55:45 +0100

    added title

--- src/containers/cards.jsx
+++ src/containers/cards.jsx
@@ -16,13 +16,19 @@ class Cards extends Component {
           className="col-md-3 shadow-sm px-0 pb-2"
           style={{ backgroundColor: this.props.cardColor }}
         >
-          <div className="w-100 mx-auto" style={{ backgroundColor: this.props.cardColor }}>
+          <div
+            className="w-100 mx-auto"
+            style={{ backgroundColor: this.props.cardColor }}
+          >
+            <p className="d-inline ml-2">
+              <b>Title</b>
+            </p>
             <AddItem />
             <DestroyCard />
           </div>
           <div className="myCard">
-            <Accordion >
-              <ListItem/>
+            <Accordion>
+              <ListItem />
             </Accordion>
           </div>
         </div>
