09b50167691e0ba4654b3555cf277b7c44212dd7
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2020-10-23 11:21:40 +0100

    Updates

---
+++ precache-manifest.a38c84a253ea4ed4a26c8878309227b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26bf60f12a69a4f3bbf45f3dda1fc27",
+    "url": "/WebBuddy/index.html"
+  },
+  {
+    "revision": "819219729cb7d39c0f7a",
+    "url": "/WebBuddy/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "8179d4e05116d86ecae2",
+    "url": "/WebBuddy/static/css/main.52ae1630.chunk.css"
+  },
+  {
+    "revision": "819219729cb7d39c0f7a",
+    "url": "/WebBuddy/static/js/2.aa90156b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/WebBuddy/static/js/2.aa90156b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8179d4e05116d86ecae2",
+    "url": "/WebBuddy/static/js/main.2be769ec.chunk.js"
+  },
+  {
+    "revision": "6a6345f99c1caa168833",
+    "url": "/WebBuddy/static/js/runtime-main.c5cfab09.js"
+  }
+]);
